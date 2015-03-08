58062531e6b22c1f1a0fa78ae0a66b22fde18fd2
Lakshman <ameex@ameex.(none)>
2015-03-08 23:05:10 +0530

    adding files again

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,15 @@
 class TasksController < ApplicationController
+  #Showing current items in db table
   def index
     @drags = Drag.order('position')
   end
 
+  #Updating positions of items
   def sort
     params[:drag].each_with_index do |id,index|
-      #Drag.update_all({position: id})
       Drag.where(:name => "item "+id).limit(1).update_all(:position => index)
     end
     render nothing: true
   end
+
 end
